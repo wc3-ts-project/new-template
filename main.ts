35d2d66118faf28c3fea6e4bfe9bacd91689e553
WriteCoin <sovtsardom@gmail.com>
2021-08-21 23:33:45 +0300

    пофиксен импорт зависимостей, для этого добавлен в tsconfig параметр baseUrl поиска путей по node_modules

---
+++ main.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+// This is necessary to bring in Lua and Ceres declarations into the scope
+// Another alternative is to put these into the `types` array in `tsconfig.json`
+/// <reference types="lua-types/5.3"/>
+/// <reference types="ceres-decl/ceres"/>
+
+import { init } from "./src/package"
+import { setTimeout } from "./src/timer"
+
+setTimeout(0, init)
