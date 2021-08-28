3323880e4105c7692e0e000045713105abfa6e08
WriteCoin <sovtsardom@gmail.com>
2021-08-28 22:44:45 +0300

    Добавлены и подправлены функции обработки потоков, добавлена реализация wait и новые типы в decl

--- src/debug.ts
+++ src/debug.ts
@@ -1,4 +1,27 @@
-const getHandledCallback = (callback: code | codeboolexpr) => () => {
+export const newThread = <T extends any>(func: () => T) => {
+    const co = coroutine.create(func)
+    const result = coroutine.resume(co)
+    if (!result[0]) {
+        print(result[1])
+        return result[0]
+    } else {
+        return result[1]
+    }
+}
+
+export const wait = (time: real) => {
+    const timer = CreateTimer()
+    const co = coroutine.running()
+
+    TimerStart(timer, time, false, () => coroutine.resume(co[0]))
+
+    coroutine.yield()
+
+    PauseTimer(timer)
+    DestroyTimer(timer)
+}
+
+const getHandledCallback = <T extends any>(callback: () => T) => () => {
     const result = pcall(callback)
     if (!result[0]) {
         print(result[1])
@@ -8,30 +31,30 @@ const getHandledCallback = (callback: code | codeboolexpr) => () => {
     }
 }
 
+const getHandledThread = (callback: code) => () => newThread(getHandledCallback(callback))
+
 export const timerStart = (
     whichTimer: timer,
     timeout: real,
     periodic: boolean,
     handlerFunc: code
-) => TimerStart(whichTimer, timeout, periodic, getHandledCallback(handlerFunc))
+) => TimerStart(whichTimer, timeout, periodic, getHandledThread(handlerFunc))
 
 export const triggerAddAction = (whichTrigger: trigger, actionFunc: code) =>
-    TriggerAddAction(whichTrigger, getHandledCallback(actionFunc))
+    TriggerAddAction(whichTrigger, getHandledThread(actionFunc))
 
-export const getCondition = (func: codeboolexpr) =>
-    Condition(getHandledCallback(func) as codeboolexpr)
+export const getCondition = (func: codeboolexpr) => Condition(getHandledThread(func))
 
-export const getFilter = (func: codeboolexpr) => Filter(getHandledCallback(func) as codeboolexpr)
+export const getFilter = (func: codeboolexpr) => Filter(getHandledThread(func))
 
 export const forForce = (whichForce: force, callback: code) =>
-    ForForce(whichForce, getHandledCallback(callback))
+    ForForce(whichForce, getHandledThread(callback))
 
 export const forGroup = (whichGroup: group, callback: code) =>
-    ForGroup(whichGroup, getHandledCallback(callback))
+    ForGroup(whichGroup, getHandledThread(callback))
 
-export const enumDestructablesInRect = (r: rect, filter: boolexpr, actionFunc: code) =>
-    EnumDestructablesInRect(r, filter, getHandledCallback(actionFunc))
+export const enumDestructablesInRect = (r: rect, filterFunc: codeboolexpr, actionFunc: code) =>
+    EnumDestructablesInRect(r, getFilter(filterFunc), getHandledThread(actionFunc))
 
-export const enumItemsInRect = (r: rect, filter: boolexpr, actionFunc: code) =>
-    EnumItemsInRect(r, filter, getHandledCallback(actionFunc))
-
+export const enumItemsInRect = (r: rect, filterFunc: codeboolexpr, actionFunc: code) =>
+    EnumItemsInRect(r, getFilter(filterFunc), getHandledThread(actionFunc))
--- src/package.ts
+++ src/package.ts
@@ -1,4 +1,18 @@
+import { getFilter, wait } from "./debug"
+
 export const init = () => {
+    wait(2)
     print(DoNothing)
     print(bj_ALLIANCE_ALLIED)
+    const group = CreateGroup()
+    GroupEnumUnitsOfPlayer(
+        group,
+        Player(0),
+        getFilter(() => GetFilterUnit() == gg_unit_Hblm_0000)
+    )
+    print(BlzGroupGetSize(group))
+    ForGroup(group, () => {
+        print(GetUnitName(GetEnumUnit()))
+    })
+    DestroyGroup(group)
 }
